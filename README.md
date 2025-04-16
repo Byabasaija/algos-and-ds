2d3be15ab89c076707345c603bc2d89874e74eca
Byabasaija <basaijapascal9@gmail.com>
2025-04-16 11:26:16 +0300

    added algos roadmap

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-# Algos and DS
-A collection of most asked algos and ds questions. Writing the algorithms from scratch. 
+## 📘 DSA Roadmap – Most Common Techniques with DS, Algorithms & Sample Problems
 
-## Why?
-For practice purposes.
+| Technique               | Related Data Structures          | Algorithms / Concepts              | Most Common Questions                                                                 |
+|------------------------|----------------------------------|------------------------------------|----------------------------------------------------------------------------------------|
+| Two Pointers           | Array, String                    | Sorting, Linear scan               | Two sum (sorted), Container with most water, Remove duplicates                         |
+| Sliding Window         | Array, HashMap                   | Variable/fixed window              | Longest substring without repeat, Max sum subarray of size k, Permutation in string   |
+| Prefix Sum             | Array, HashMap                   | Cumulative sum                     | Subarray sum equals k, Range sum query, Find equilibrium index                         |
+| Hashing                | HashMap, Set                     | Frequency counting                 | Group anagrams, Top k frequent elements, Happy number                                  |
+| Basic Recursion        | Stack (implicit), Array          | Divide problem into subproblems    | Factorial, Fibonacci, Sum of digits                                                    |
+| Backtracking           | Array, Grid, Recursion tree      | Recursion tree + pruning           | Subsets, N-Queens, Word search                                                         |
+| Binary Search          | Array (sorted)                   | Classic BS, BS on answer           | Search in rotated array, First/last position of element, Koko eating bananas           |
+| Greedy                 | Array, Heap, Sorting             | Local optimal → global optimal     | Jump game, Activity selection, Gas station                                             |
+| Dynamic Programming    | Array, String, Matrix            | Memoization, Tabulation            | 0/1 Knapsack, LIS, House robber, Edit distance                                         |
+| Kadane’s Algorithm     | Array                            | DP (running max)                   | Maximum subarray sum, Max circular subarray                                            |
+| Fast & Slow Pointers   | Linked List                      | Floyd's cycle detection            | Detect cycle in linked list, Find middle, Start of cycle                               |
+| DFS / BFS              | Graph (Adj. list/matrix), Grid   | Graph traversal                    | Number of islands, Clone graph, Course schedule (cycle detection)                     |
+| Union-Find (DSU)       | Array                            | Path compression, Union by rank    | Accounts merge, Friend circles, Number of provinces                                    |
+| Heap / Priority Queue  | Array (Heap), List               | Min/max heap operations            | K closest points, Merge k sorted lists, Top k frequent elements                        |
+| Trie + Backtracking    | Trie tree                        | Prefix matching, DFS               | Word search II, Replace words, Auto-complete suggestions                               |
+| Topological Sort       | Directed Graph, Queue/Stack      | Kahn’s/DFS topological order       | Course schedule, Alien dictionary, Build order                                         |
+
--- Sorting algorithms/BubbleSort.py
+++ Sorting algorithms/BubbleSort.py
@@ -1,64 +1,106 @@
 
 
-def simple_bubble(unsorted):
-    """***Thought process***
-    - We compare two pairs of the elements in the list switching them if the first is greatr than the 2nd pair
-    - we therefore run through the list until we reach the second last element since there is no more pairs
-    - we initialize a sorted variable to false. this will help break out od the loop in case we have a fully sorted list
-    - we will use while loop to until the sorted is true
-    - in the while loop, we immediatly set sorted to true by default
-    - then create a for loop through the list, them compare elements at index and index+1
-    - if they need swaping, we do the swap, set sorted to false and continue in the loop
-    - If there are no more swaps needed, we set sorted to true and exit the will loop
-    - return the list
+# def simple_bubble(unsorted):
+#     """***Thought process***
+#     - We compare two pairs of the elements in the list switching them if the first is greatr than the 2nd pair
+#     - we therefore run through the list until we reach the second last element since there is no more pairs
+#     - we initialize a sorted variable to false. this will help break out od the loop in case we have a fully sorted list
+#     - we will use while loop to until the sorted is true
+#     - in the while loop, we immediatly set sorted to true by default
+#     - then create a for loop through the list, them compare elements at index and index+1
+#     - if they need swaping, we do the swap, set sorted to false and continue in the loop
+#     - If there are no more swaps needed, we set sorted to true and exit the will loop
+#     - return the list
 
-    """
-    i_length = len(unsorted) - 1
-    is_sorted = False
-    while is_sorted == False:
-        is_sorted = True
-        for i in range(0, i_length):
-            if unsorted[i] > unsorted[i+1]:
-                is_sorted = False
-                unsorted[i], unsorted[i+1] = unsorted[i+1], unsorted[i]
-    return unsorted
+#     """
+#     i_length = len(unsorted) - 1
+#     is_sorted = False
+#     while is_sorted == False:
+#         is_sorted = True
+#         for i in range(0, i_length):
+#             if unsorted[i] > unsorted[i+1]:
+#                 is_sorted = False
+#                 unsorted[i], unsorted[i+1] = unsorted[i+1], unsorted[i]
+#     return unsorted
                     
 
 
 
-def students_list( scores, student_ids):
+# def students_list( scores, student_ids):
 
-    """Thought process 
-    We map student ids against  their corresponding scores
-    We come up with a dictionary where each id has a list of scores
-    we iterate the dictionary items and sort the values indipendently
-    we later join the sorted lists returned by the bubble sort
+#     """Thought process 
+#     We map student ids against  their corresponding scores
+#     We come up with a dictionary where each id has a list of scores
+#     we iterate the dictionary items and sort the values indipendently
+#     we later join the sorted lists returned by the bubble sort
 
 
-    """
+#     """
 
-    list_dict = {}
-    for student_id, score in zip(student_ids, scores):
-        if student_id in list_dict:
-            list_dict[student_id].append(score)
-        else:
-            list_dict[student_id] = [score]
+#     list_dict = {}
+#     for student_id, score in zip(student_ids, scores):
+#         if student_id in list_dict:
+#             list_dict[student_id].append(score)
+#         else:
+#             list_dict[student_id] = [score]
  
 
-    sorted_list = []
-    for key, value in list_dict.items():
-        sorted_list.extend(simple_bubble(value))
+#     sorted_list = []
+#     for key, value in list_dict.items():
+#         sorted_list.extend(simple_bubble(value))
 
     
     
-    return print(sorted_list)
+#     return print(sorted_list)
 
             
                 
-### TESTING 
+# ### TESTING 
 
-# unsorted =  [1,3,7,4,8,10,2,2,1]
-# simple_bubble(unsorted=unsorted)
-scores = [50, 40, 60, 30, 70, 20]
-student_ids = [1, 1, 1, 2, 2, 2]
-students_list(scores=scores, student_ids=student_ids)
+# # unsorted =  [1,3,7,4,8,10,2,2,1]
+# # simple_bubble(unsorted=unsorted)
+# scores = [50, 40, 60, 30, 70, 20]
+# student_ids = [1, 1, 1, 2, 2, 2]
+# students_list(scores=scores, student_ids=student_ids)
+
+
+
+# """
+# A function to do bubble sort takes in a list/array of integers to be sorted(or whatever)
+# iterates on the list
+# the iteration is supposed to stop at the second last element, since we are picking pairs. 
+# We pick a pair on each iteration, compare the two elements and rearrange them
+
+# """
+
+# def bubbleSort(lst: list):
+#     current_index = 1
+
+#     for i in range((len(lst) - 1)):
+#         pass
+
+
+def palindrome(num:list | str):
+    if num[::-1] == num:
+        print("Yes")
+    else:
+        print("no")
+    
+    
+pal = palindrome([333])
+
+## with two pointers technique
+
+def twoPointerPal(num):
+    left = 0
+    right = len(num)-1
+
+    for i in range(len(num)):
+        if num[left] != num[right]:
+            return False
+        right -=1
+        left +=1
+    return True
+        
+print(twoPointerPal("racecar"))
+    
